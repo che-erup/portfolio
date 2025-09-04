aa1a9df2eca9766f5b0ffd3761c91c38fd2f1931
che-erup <redners@tistory.com>
2025-09-04 16:05:45 +0900

    25.09.04

--- src/data/ProjectData.js
+++ src/data/ProjectData.js
@@ -1,38 +1,133 @@
 // --------------------------------------- Project List
 export const ProjectList = [
   {
-    img: "https://raw.githubusercontent.com/gurupawar/website/main/src/Assets/project_1.png",
-    title: "Spense",
-    description: "Codewell challenge, Spense is a landing page for writers.",
-    tech_stack: "Html, Css, Javascript",
+    img: "https://i.postimg.cc/BbqPNbgC/diaspora-thumb.webp",
+    title: "음악 디깅 유튜브,
+    description: "인디 음악을 대중들에게 소개하는 유튜브 채널\n",
+    tech_stack: "조사(100%), 기획(100%), 편집(100%), 그래픽(100%)",
+    genre: "유튜브",
+    /* 
     github_url: "https://your_project_github_link",
-    demo_url: "https://your_project_demo_link",
+    */
+    demo_url: "https://www.youtube.com/watch?v=v6A2w5taqtk&t=394s",
+    modal: {
+      video: "https://www.youtube.com/embed/v6A2w5taqtk?si=hrMGsZTffbY-BF01",
+      summary: "유튜브 채널에 대한 추가 설명입니다."
+    }
   },
   {
-    img: "https://raw.githubusercontent.com/gurupawar/website/main/src/Assets/project_2.png",
-    title: "Fiber",
+    img: "https://i.postimg.cc/BbqPNbgC/diaspora-thumb.webp",
+    title: "섬마을 디아스포라 (2024)",
+    description: "섬마을로 다시 돌아온 한 어부의 삶을 통해,\n기존 '디아스포라'의 개념을 '도메스틱 디아스포라'까지 \n새롭게 확장하는 단편 다큐멘터리",
+    tech_stack: "연출, 제작, 촬영(100% 1인제작)",
+    genre: "단편 다큐멘터리",
+    /* 
+    github_url: "https://your_project_github_link",
+    */
+    demo_url: "https://www.youtube.com/watch?v=BRflVt5U_Fs",
+    modal: {
+      video: "https://www.youtube.com/embed/BRflVt5U_Fs",
+      summary: "섬마을 디아스포라에 대한 추가 설명입니다."
+    }
+  },
+  {
+    img: "https://i.postimg.cc/9fbQRzn7/2025-05-14-11-19-24.png",
+    title: "치도마을 아카이브 영상 (2024)",
     description:
-      "An online portfolio generator. Great to practice flex/grid layouts, absolute positioning and sliders.",
-    tech_stack: "Html, Css, Javascript",
-    github_url: "https://your_project_github_link",
-    demo_url: "https://your_project_demo_link",
+      "해양수산부 주관의 어촌뉴딜 사업 중, 영상 외주제작을 진행. \n 전라북도 부안군의 치도라는 작은 섬마을의 현재를 기록하는 아카이브 영상을 \n집단 인터뷰 형식으로 제작.",
+    genre: "외주영상",
+    tech_stack: "연출(100%), 제작(100%), 촬영(80%), 편집(100%)",
+    demo_url: "https://youtu.be/Ozky_YNhgEA?si=K6UPJ54odRbHBu54",
+    modal: {
+      video: "https://www.youtube.com/embed/Ozky_YNhgEA",
+      summary: "치도마을 홍보영상에 대한 추가 설명입니다."
+    }
   },
   {
-    img: "https://raw.githubusercontent.com/gurupawar/website/main/src/Assets/portfolio.png",
-    title: "Portfolio",
-    description: "Ofcourse i made my own personal portfolio website.",
-    tech_stack: "Reactjs, Css",
-    github_url: "https://your_project_github_link",
-    demo_url: "https://your_project_demo_link/",
-  },
-  {
-    img: "https://raw.githubusercontent.com/gurupawar/website/main/src/Assets/project_3.png",
-    title: "Ecommerce website",
+    img: "https://i.postimg.cc/Sswvnc77/2025-05-14-10-12-38.png",
+    title: "치도마을 홍보영상 (2024)",
     description:
-      "Created simple ecommerce website using reactjs and react-use-cart plugin.",
-    tech_stack: "Reactjs, Css",
-    github_url: "https://your_project_github_link",
+      "해양수산부 주관의 어촌뉴딜 사업 중, 영상 외주제작을 진행. \n 전라북도 부안군의 치도라는 작은 섬마을을 다각적으로 홍보하는 홍보영상을 제작.",
+    genre: "외주영상",
+    tech_stack: "제작(50%))",
+    demo_url: "https://youtu.be/Ozky_YNhgEA?si=K6UPJ54odRbHBu54",
+    modal: {
+      video: "https://www.youtube.com/embed/Ozky_YNhgEA",
+      summary: "치도마을 홍보영상에 대한 추가 설명입니다."
+    }
+  },
+  {
+    img: "https://i.postimg.cc/QNBwtb0r/jiraigen-thumb.jpg",
+    title: "지뢰밭 러버즈 (2023)",
+    description:
+      "자살을 원하는 한국인 남자와 일본인 여자가 만나고, \n동반자살 여행을 떠나는 이야기. \n 일본 현지에서 캐스팅과 촬영을 직접 진행.",
+    genre: "단편 모큐멘터리",
+    tech_stack: "연출(100%), 제작(100%), 촬영(100%), 편집(100%), 연기(30%)",
+    demo_url: "https://www.youtube.com/embed/xUeqOGYzvxw?si=Zfu_U3J3-8NN2mu0",
+    modal: {
+      video: "https://www.youtube.com/embed/xUeqOGYzvxw?si=Zfu_U3J3-8NN2mu0",
+      summary: "영상에 대한 추가 설명입니다."
+    }
+  },
+  {
+    img: "http://file.koreafilm.or.kr/still/copy/00/65/98/DST832735_01.jpg",
+    title: "그로기 (2023)",
+    description: "세진은 대부업체 수금책이다. \n 어느 날 세진은 수금을 하러 간 권투장에서 한 여자를 만나고 조금씩 변해간다.",
+    genre: "단편영화",
+    tech_stack: "제작(100%)",
+    demo_url: "https://www.youtube.com/watch?v=hzqSUZKcr2k",
+    modal: {
+      video: "https://www.youtube.com/embed/hzqSUZKcr2k?si=f17eHjTNTYutgzqc",
+      summary: "영상에 대한 추가 설명입니다."
+    }
+  },
+  {
+    img: "https://i.postimg.cc/VNMp9rSf/badblood-thumb.jpg",
+    title: "나쁜 피 (2023)",
+    description:
+      "선우는 트랜스젠더다. 선우는 성전환수술을 위해 보호자의 동의를 받기 위해 오래 전 연락이 끊긴 아버지를 찾기로 한다. \n그러나 겨우 찾아낸 아버지 또한 트랜스젠더였고, 불법투여한 호르몬제의 부작용으로 생명이 위독한 상태였다.",
+    genre: "단편영화",
+    tech_stack: "연출(100%), 제작(80%), 편집(100%)",
+    demo_url: "https://www.youtube.com/embed/Hx2-uAkhxmo?si=CxNt6KEtOLCyQ0Gp",
+    modal: {
+      video: "https://www.youtube.com/embed/Hx2-uAkhxmo?si=CxNt6KEtOLCyQ0Gp",
+      summary: "영상에 대한 추가 설명입니다."
+    }
+  },
+  {
+    img: "https://i.postimg.cc/XJHQ5dfD/2025-05-14-10-46-11.png",
+    title: "실낙원 (2021)",
+    description:
+      "낙원상가는 어떻게 실낙원이 되었는가.",
+    genre: "비디오에세이",
+    tech_stack: "연출(100%), 촬영(100%), 편집(100%)",
     demo_url: "https://your_project_demo_link",
+    modal: {
+      video: "https://player.vimeo.com/video/642795044?h=a42dced3b0",
+      summary: "영상에 대한 추가 설명입니다."
+    }
+  },
+  {
+    img: "https://i.postimg.cc/QNBwtb0r/jiraigen-thumb.jpg",
+    title: "그 외 단편영화들",
+    description:
+      "단편영화 「절」 (2020) 촬영 \n단편영화 「클린업」 (2022) 제작 \n단편영화 「중고나라」 (2016) 연출,제작,편집\n단편영화 「밥은 혼자 먹어야 제맛」 (2015) 연출,편집 \n단편영화 「이사」 (2015) 촬영",      
+    genre: "단편영화",
+    tech_stack: "연출, 제작, 촬영, 편집",
+    noModal: true,
+    noGenre: true,
+    noTechStack: true,
+  },
+  {
+    img: "https://www.nhk.jp/static/assets/images/tvepisode/te/YX6GKKNGN4/YX6GKKNGN4-eyecatch_59dc0ccca0b522a3e41457c361357c55.jpg",
+    title: "방송촬영",
+    description:
+      "NHK ニュース (2024.12.06)\nNHK クローズアップ現代 (2024.12.18)\nNHK 国際報道 2025 (2025.04.18)\nテレ朝 '韓国 尹大統領の罷免決定　厳戒態勢続く現地は…'(2025.04.04)\nテレ朝 '韓国・憲法裁判所「国民の信頼を裏切った」　尹大統領「罷免」判決'(2025.04.04)\n \n등 일본 방송국 한국 국내 촬영",      
+    genre: "단편영화",
+    tech_stack: "촬영(30%), 통역(20%)",
+    noModal: true,
+    noGenre: true,
+    noTechStack: true,
   },
 ];
 
@@ -40,35 +135,28 @@ export const ProjectList = [
 
 export const stackList = [
   {
-    img: "https://raw.githubusercontent.com/gurupawar/website/main/src/Assets/skill/html5.svg",
-    name: "HTML",
+    img: "https://i.postimg.cc/CMB6tQJd/japan-flag-circular-17764.png",
+    name: "JLPT N1",
   },
   {
-    img: "https://raw.githubusercontent.com/gurupawar/website/main/src/Assets/skill/css3.svg",
-    name: "CSS",
+    img: "https://i.postimg.cc/gj8s01BD/video-camera-2806.png",
+    name: "Cinema Camera",
   },
   {
-    img: "https://raw.githubusercontent.com/gurupawar/website/main/src/Assets/skill/javascript.svg",
-    name: "JavaScript",
+    img: "https://i.postimg.cc/gj8s01BD/video-camera-2806.png",
+    name: "6mm Camera",
   },
   {
-    img: "https://raw.githubusercontent.com/gurupawar/website/main/src/Assets/skill/react.svg",
-    name: "ReactJS",
+    img: "https://i.postimg.cc/5ydGpBhs/camera-1429.png",
+    name: "DSLR",
   },
   {
-    img: "https://raw.githubusercontent.com/gurupawar/website/main/src/Assets/skill/bootstrap.svg",
-    name: "Bootstrap",
+    img: "https://help.apple.com/assets/673BE5C0E115654F7F097772/673BE5C41BAE7922D30F2CE1/ko_KR/97f5f4dfe6df84d78caacff68ec63538.png",
+    name: "Final cut",
   },
   {
-    img: "https://raw.githubusercontent.com/gurupawar/website/main/src/Assets/skill/sass.svg",
-    name: "Scss",
+    img: "https://upload.wikimedia.org/wikipedia/commons/thumb/9/90/DaVinci_Resolve_17_logo.svg/130px-DaVinci_Resolve_17_logo.svg.png?20211228192035",
+    name: "Davinci resolve",
   },
-  {
-    img: "https://raw.githubusercontent.com/gurupawar/website/main/src/Assets/skill/git.svg",
-    name: "Git",
-  },
-  {
-    img: "https://raw.githubusercontent.com/gurupawar/website/main/src/Assets/skill/responsive.svg",
-    name: "Responsive",
-  },
+
 ];
