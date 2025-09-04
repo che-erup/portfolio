40e838dc9e3767985361d459c66d43f4c083cb66
che-erup <redners@tistory.com>
2025-09-04 16:13:00 +0900

    25.09.04 수정

--- src/data/ProjectData.js
+++ src/data/ProjectData.js
@@ -2,9 +2,24 @@
 export const ProjectList = [
   {
     img: "https://i.postimg.cc/BbqPNbgC/diaspora-thumb.webp",
+    title: "음악 디깅 유튜브,
+    description: "인디 음악을 대중들에게 소개하는 유튜브 채널\n",
+    tech_stack: "조사(100%), 기획(100%), 편집(100%), 그래픽(100%)",
+    genre: "유튜브",
+    /* 
+    github_url: "https://your_project_github_link",
+    */
+    demo_url: "https://www.youtube.com/watch?v=v6A2w5taqtk&t=394s",
+    modal: {
+      video: "https://www.youtube.com/embed/v6A2w5taqtk?si=hrMGsZTffbY-BF01",
+      summary: "유튜브 채널에 대한 추가 설명입니다."
+    }
+  },
+  {
+    img: "https://i.postimg.cc/BbqPNbgC/diaspora-thumb.webp",
     title: "섬마을 디아스포라 (2024)",
     description: "섬마을로 다시 돌아온 한 어부의 삶을 통해,\n기존 '디아스포라'의 개념을 '도메스틱 디아스포라'까지 \n새롭게 확장하는 단편 다큐멘터리",
-    tech_stack: "연출, 제작 촬영",
+    tech_stack: "연출, 제작, 촬영(100% 1인제작)",
     genre: "단편 다큐멘터리",
     /* 
     github_url: "https://your_project_github_link",
@@ -21,7 +36,7 @@ export const ProjectList = [
     description:
       "해양수산부 주관의 어촌뉴딜 사업 중, 영상 외주제작을 진행. \n 전라북도 부안군의 치도라는 작은 섬마을의 현재를 기록하는 아카이브 영상을 \n집단 인터뷰 형식으로 제작.",
     genre: "외주영상",
-    tech_stack: "연출, 제작, 촬영, 편집",
+    tech_stack: "연출(100%), 제작(100%), 촬영(80%), 편집(100%)",
     demo_url: "https://youtu.be/Ozky_YNhgEA?si=K6UPJ54odRbHBu54",
     modal: {
       video: "https://www.youtube.com/embed/Ozky_YNhgEA",
@@ -34,7 +49,7 @@ export const ProjectList = [
     description:
       "해양수산부 주관의 어촌뉴딜 사업 중, 영상 외주제작을 진행. \n 전라북도 부안군의 치도라는 작은 섬마을을 다각적으로 홍보하는 홍보영상을 제작.",
     genre: "외주영상",
-    tech_stack: "제작, 촬영",
+    tech_stack: "제작(50%))",
     demo_url: "https://youtu.be/Ozky_YNhgEA?si=K6UPJ54odRbHBu54",
     modal: {
       video: "https://www.youtube.com/embed/Ozky_YNhgEA",
@@ -47,7 +62,7 @@ export const ProjectList = [
     description:
       "자살을 원하는 한국인 남자와 일본인 여자가 만나고, \n동반자살 여행을 떠나는 이야기. \n 일본 현지에서 캐스팅과 촬영을 직접 진행.",
     genre: "단편 모큐멘터리",
-    tech_stack: "연출, 제작, 촬영, 편집, 연기",
+    tech_stack: "연출(100%), 제작(100%), 촬영(100%), 편집(100%), 연기(30%)",
     demo_url: "https://www.youtube.com/embed/xUeqOGYzvxw?si=Zfu_U3J3-8NN2mu0",
     modal: {
       video: "https://www.youtube.com/embed/xUeqOGYzvxw?si=Zfu_U3J3-8NN2mu0",
@@ -59,7 +74,7 @@ export const ProjectList = [
     title: "그로기 (2023)",
     description: "세진은 대부업체 수금책이다. \n 어느 날 세진은 수금을 하러 간 권투장에서 한 여자를 만나고 조금씩 변해간다.",
     genre: "단편영화",
-    tech_stack: "제작",
+    tech_stack: "제작(100%)",
     demo_url: "https://www.youtube.com/watch?v=hzqSUZKcr2k",
     modal: {
       video: "https://www.youtube.com/embed/hzqSUZKcr2k?si=f17eHjTNTYutgzqc",
@@ -72,7 +87,7 @@ export const ProjectList = [
     description:
       "선우는 트랜스젠더다. 선우는 성전환수술을 위해 보호자의 동의를 받기 위해 오래 전 연락이 끊긴 아버지를 찾기로 한다. \n그러나 겨우 찾아낸 아버지 또한 트랜스젠더였고, 불법투여한 호르몬제의 부작용으로 생명이 위독한 상태였다.",
     genre: "단편영화",
-    tech_stack: "연출, 제작, 편집",
+    tech_stack: "연출(100%), 제작(80%), 편집(100%)",
     demo_url: "https://www.youtube.com/embed/Hx2-uAkhxmo?si=CxNt6KEtOLCyQ0Gp",
     modal: {
       video: "https://www.youtube.com/embed/Hx2-uAkhxmo?si=CxNt6KEtOLCyQ0Gp",
@@ -85,7 +100,7 @@ export const ProjectList = [
     description:
       "낙원상가는 어떻게 실낙원이 되었는가.",
     genre: "비디오에세이",
-    tech_stack: "연출, 촬영, 편집",
+    tech_stack: "연출(100%), 촬영(100%), 편집(100%)",
     demo_url: "https://your_project_demo_link",
     modal: {
       video: "https://player.vimeo.com/video/642795044?h=a42dced3b0",
@@ -109,7 +124,7 @@ export const ProjectList = [
     description:
       "NHK ニュース (2024.12.06)\nNHK クローズアップ現代 (2024.12.18)\nNHK 国際報道 2025 (2025.04.18)\nテレ朝 '韓国 尹大統領の罷免決定　厳戒態勢続く現地は…'(2025.04.04)\nテレ朝 '韓国・憲法裁判所「国民の信頼を裏切った」　尹大統領「罷免」判決'(2025.04.04)\n \n등 일본 방송국 한국 국내 촬영",      
     genre: "단편영화",
-    tech_stack: "연출, 제작, 촬영, 편집",
+    tech_stack: "촬영(30%), 통역(20%)",
     noModal: true,
     noGenre: true,
     noTechStack: true,
